52594deae161b967dd3811a49b0fa5c85e5a16bc
Gise L <gisellalopez09@gmail.com>
2023-10-01 02:03:41 -0300

    Versión Final

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+// Animaciones que no funcionan... Can't say I haven't tried tough
+AOS.init({
+  duration: 1000
+});
+
+  document.getElementById("boton-sobremi").addEventListener("click", function(){
+    document.getElementById("personal").style.display="block";
+  });
